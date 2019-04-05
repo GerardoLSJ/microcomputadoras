539d20cdfc48c90f7affb9fc0ad38cfc8619509f
Gerardo LSJ <gerardolsj@gmail.com>
2019-04-05 00:25:41 -0600

    TEST

--- p2/e1/clase15marzo_FIX.asm
+++ p2/e1/clase15marzo_FIX.asm
@@ -1,5 +1,5 @@
-	processor 16f877A
-	include <p16f877A.inc>
+    processor 16f877A
+    include <p16f877A.inc>
 
 entrada equ 0x30
 
@@ -14,145 +14,145 @@ aux equ 0x32
 cte1 equ 50h
 cte2 equ 51h
 cte3 equ 52h
-	
-	org 0
-	goto inicio
-	org 5
+
+    org 0
+    goto inicio
+    org 5
 
 
 inicio
-	;condiciones iniciales
-	bsf STATUS, 5
-	;PUERTO E COMO ENTRADA XXXXX111
-	movlw 0x07
-	movwf TRISE
-	;PUERTO B COMO SALIDA 00000000
-	movlw 0x00
-	movwf TRISB
-	movlw 0x07
-	movwf ADCON1
-	movlw 0x00
-	movwf flag
+    ; condiciones iniciales
+    bsf STATUS, 5
+    ; PUERTO E COMO ENTRADA XXXXX111
+    movlw 0x07
+    movwf TRISE
+    ; PUERTO B COMO SALIDA 00000000
+    movlw 0x00
+    movwf TRISB
+    movlw 0x07
+    movwf ADCON1
+    movlw 0x00
+    movwf flag
 
 lecturaEntrada
-	bcf STATUS,5
-	movf PORTE,0
-	movwf entrada
-	movlw 0x07
-	andwf entrada,1
-	movlw 0x01
-	xorwf entrada,0
-	btfsc STATUS,Z
-	goto corrimientoD
-	movlw 0x02
-	xorwf entrada,0
-	btfsc STATUS,Z
-	goto corrimientoI
-	movlw 0x03
-	xorwf entrada,0
-	btfsc STATUS,Z
-	goto centro
-	call parpadeo
+    bcf STATUS,5
+    movf PORTE,0
+    movwf entrada
+    movlw 0x07
+    andwf entrada,1
+    movlw 0x01
+    xorwf entrada,0∫
+    ; btfsc STATUS,Z  ; comment siempre entra a D TEST
+    goto corrimientoD
+
+    movlw 0x02
+    xorwf entrada,0
+    btfsc STATUS,Z
+    goto corrimientoI
+
+    movlw 0x03
+    xorwf entrada,0
+    btfsc STATUS,Z
+    goto centro
+
+    call parpadeo
 
 corrimientoD
-	bcf STATUS,C
-	movlw 0x80
-	;;
-	btfss flag, 0
-	movf aux, 0	
-	;;
+    bcf STATUS,C
+    movlw 0x80
+    ; ;
+    btfss flag, 0     ; bit test skip if set
+    movf aux, 0       ; cargar en registro W
 
-	movwf PORTB ;PORTB es salida
-	call retardo
-	rrf PORTB,1
-	call retardo
-	
-	;;
-	movf PORTB, 0
-	movwf aux
-	;;
-	movlw 0x00
-	movwf flag
+    movwf PORTB       ; PORTB es salida
+    call retardo
+    rrf PORTB,1
+    call retardo
+
+    ; ;
+    movf PORTB, 0
+    movwf aux
+    ; ;
+    movlw 0x00
+    movwf flag
 
 
-	btfss PORTB,0 ;; YA ACABO SU CORRIMIENTO
-	goto changeFlagOn
+    btfss PORTB,0     ; YA ACABO SU CORRIMIENTO
+    goto changeFlagOn
 
-	;goto corrimientoD
+    goto lecturaEntrada
 
-	goto lecturaEntrada	
-	
 corrimientoI
-	bcf STATUS,C
-	movlw 0x01
-	movwf PORTB ;PORTB
-	call retardo
-loop2	
-	rlf PORTB,1
-	call retardo
-	btfss PORTB,7 ; YA ACABO SU CORRIMIENTO VE A LEER
-	goto loop2
-	goto lecturaEntrada
+    bcf STATUS,C
+    movlw 0x01
+    movwf PORTB       ; PORTB
+    call retardo
+loop2
+    rlf PORTB,1
+    call retardo
+    btfss PORTB,7     ; YA ACABO SU CORRIMIENTO VE A LEER
+    goto loop2
+    goto lecturaEntrada
 
-;;;;;;;;;;
+    ; ----
 
 centro
-	movlw 0x01
-	movwf flag
+    movlw 0x01
+    movwf flag
 
-	clrf 0x20
+    clrf 0x20
 loop3
-	movf 0x20,0
-	call tablaValores
-	incf 0x20,1
-	movwf PORTB
-	call retardo
-	btfss PORTB,4
-	goto loop3
-	goto lecturaEntrada 
+    movf 0x20,0
+    call tablaValores
+    incf 0x20,1
+    movwf PORTB
+    call retardo
+    btfss PORTB,4
+    goto loop3
+    goto lecturaEntrada
 
 parpadeo
-	movlw 0x01
-	movwf flag
+    movlw 0x01
+    movwf flag
 
-	movlw 0xFF
-	movwf PORTB
-	call retardo
-	movlw 0x00
-	movwf PORTB
-	call retardo
-	goto lecturaEntrada
+    movlw 0xFF
+    movwf PORTB
+    call retardo
+    movlw 0x00
+    movwf PORTB
+    call retardo
+    goto lecturaEntrada
 
 
 tablaValores
-	addwf PCL,1
-	retlw 0x81
-	retlw 0x42
-	retlw 0x24
-	retlw 0x18
+    addwf PCL,1
+    retlw 0x81
+    retlw 0x42
+    retlw 0x24
+    retlw 0x18
 
-retardo 
-	movlw cte1 
-	movwf valor1 
-tres  
-	movlw cte2 
-	movwf valor2 
-dos  
-	movlw cte3 
-	movwf valor3 
-uno      
-	decfsz valor3 
-	goto uno 
-	decfsz valor2 
-	goto dos 
-	decfsz valor1 
-	goto tres 
-	return
-	
+retardo
+    movlw cte1
+    movwf valor1
+tres
+    movlw cte2
+    movwf valor2
+dos
+    movlw cte3
+    movwf valor3
+uno
+    decfsz valor3
+    goto uno
+    decfsz valor2
+    goto dos
+    decfsz valor1
+    goto tres
+    return
+
 
 changeFlagOn
-	movlw 0x01
-	movwf flag 
-	goto lecturaEntrada	
+    movlw 0x01
+    movwf flag
+    goto lecturaEntrada
 
 END
--- p2/e2/suma7Seg.asm
+++ p2/e2/suma7Seg.asm
@@ -1,12 +1,12 @@
-	processor 16f877
-	include <p16f877.inc>
+    processor 16f877
+    include <p16f877.inc>
 
 entrada equ 0x30
 dato1 equ 0x31
 dato2 equ 0x32
 resultado equ 0x33
 
-;;;;;;;;;;;;;;;;;
+; ;;;;;;;;;;;;;;;;
 contador equ 0x20
 valor1 equ h'21'
 valor2 equ h'22'
@@ -15,85 +15,85 @@ valor3 equ h'23'
 cte1 equ 50h
 cte2 equ 51h
 cte3 equ 52h
-;;;;;;;;;;;;;;;;;
-	
-	org 0
-	goto inicio
-	org 5
+    ; ;;;;;;;;;;;;;;;;
+
+    org 0
+    goto inicio
+    org 5
 
 inicio
-	;condiciones iniciales
-	bsf STATUS, 5
-	PUERTO A COMO ENTRADA XXXXX111
-	movlw 0x07
-	movwf TRISAmovwf dato1
-	PUERTO B COMO SALIDA 00000000
-	movlw 0x00
-	movwf TRISB
-	movlw 0x07
-	movwf ADCON1
+    ; condiciones iniciales
+    bsf STATUS, 5
+    ; PUERTO A COMO ENTRADA XXXXX111
+    movlw 0x07
+    movwf TRISAmovwf dato1
+    PUERTO B COMO SALIDA 00000000
+    movlw 0x00
+    movwf TRISB
+    movlw 0x07
+    movwf ADCON1
 
 lecturaEntrada
-	bcf STATUS, 5
-	movf PORTA, 0
-	
-	; MSB
-	movwf entrada
-	movlw 0xF0
-	andwf entrada, 0
-	movwf dato1
+    bcf STATUS, 5
+    movf PORTA, 0
 
-	; LSB
-	movwf entrada
-	movlw 0x0F
-	andwf entrada, 0
-	movwf dato2
+    ; MSB
+    movwf entrada
+    movlw 0xF0
+    andwf entrada, 0
+    movwf dato1
 
-	goto suma
+    ; LSB
+    movwf entrada
+    movlw 0x0F
+    andwf entrada, 0
+    movwf dato2
+
+    goto suma
 
 suma
-	movf dato1, 0
-	addwf dato2, 0
-	movwf resultado
-	
-	call tablaValores
+    movf dato1, 0
+    addwf dato2, 0
+    movwf resultado
+
+    call tablaValores
 
 tablaValores
-	addwf PCL,1
+    addwf PCL,1
 
-	retlw 0x7E	;0
-	retlw 0x60	;1
-	retlw 0x5B	;2
-	retlw 0x73	;3
-	retlw 0x65	;4
-	retlw 0x37	;5
-	retlw 0x3F	;6
-	retlw 0x63	;7
-	retlw 0x7F	;8
-	retlw 0x67	;9
-	retlw 0x6F	;A
-	retlw 0x3D	;B
-	retlw 0x1E	;C
-	retlw 0x79	;D
-	retlw 0x1F	;E
-	retlw 0x0F	;F
+    retlw 0x7E       ; 0
+    retlw 0x60       ; 1
+    retlw 0x5B       ; 2
+    retlw 0x73       ; 3
+    retlw 0x65       ; 4
+    retlw 0x37       ; 5
+    retlw 0x3F       ; 6
+    retlw 0x63       ; 7
+    retlw 0x7F       ; 8
+    retlw 0x67       ; 9
+    retlw 0x6F       ; A
+    retlw 0x3D       ; B
+    retlw 0x1E       ; C
+    retlw 0x79       ; D
+    retlw 0x1F       ; E
+    retlw 0x0F       ; F
 
-retardo 
-	movlw cte1 
-	movwf valor1 
-tres  
-	movlw cte2 
-	movwf valor2 
-dos  
-	movlw cte3 
-	movwf valor3 
-uno      
-	decfsz valor3 
-	goto uno 
-	decfsz valor2 
-	goto dos 
-	decfsz valor1 
-	goto tres 
-	return
-	
-	END
+retardo
+    movlw cte1
+    movwf valor1
+tres
+    movlw cte2
+    movwf valor2
+dos
+    movlw cte3
+    movwf valor3
+uno
+    decfsz valor3
+    goto uno
+    decfsz valor2
+    goto dos
+    decfsz valor1
+    goto tres
+    return
+
+    END
